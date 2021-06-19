8402a5b62bc0e141c15bf24efbf436aee681cf24
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:58:06 +0200

    sm6250-common: Move excluded-input-devices.xml to vibrator folder
    Change-Id: I6f384c556e124052b3695a87f225207dae7c8089

--- common.mk
+++ common.mk
@@ -379,7 +379,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi_sm6250
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_COPY_FILES += \
